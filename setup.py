ebdd43cdf26d4e1452540dca39bc884f6c2277e6
Eugene Prilepin <esp.home@gmail.com>
2019-11-30 01:11:57 +0300

    Change python_requires to >=3.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     name='click-option-group',
     version=get_version(),
     packages=find_packages(exclude='tests'),
-    python_requires='>=3.5,<4',
+    python_requires='>=3.6,<4',
     install_requires=[
         'Click>=7.0',
     ],
