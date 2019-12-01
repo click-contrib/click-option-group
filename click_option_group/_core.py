fba272d0a0ac926a5dab24495964d10e2a267708
Eugene Prilepin <esp.home@gmail.com>
2019-12-02 00:09:16 +0300

    Improve help for option group and group's options
    Add fake option that is used as group help record (name, extra and help)

--- click_option_group/_core.py
+++ click_option_group/_core.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import typing as ty
+import random
+import string
 import weakref
 
 import click
 from click.core import augment_usage_errors
+
+
+_FAKE_OPT_NAME_LEN = 30
 
 
 class GroupedOption(click.Option):
@@ -39,32 +44,30 @@ class GroupedOption(click.Option):
     def get_help_record(self, ctx: click.Context):
         opts, opt_help = super().get_help_record(ctx)
 
-        group_option_names = list(self.group.get_options(ctx))
-        group_option_names.reverse()
+        fake_option = self.group.get_fake_option(ctx)
+        if not fake_option:
+            return opts, opt_help
 
-        indent_inc = ctx.make_formatter().indent_increment
-        indent = ' ' * indent_inc
-
-        if self.name == group_option_names[0]:
-            group_name, group_help = self.group.get_help_record(ctx)
-            opts = f'\n{indent}{group_name}:\n{indent*2}{opts}'
-            opt_help = f'{opt_help}'
+        if self.name == fake_option.name:
+            return self.group.get_help_record(ctx)
         else:
-            opts = f'{indent}{opts}'
-
-        return opts, opt_help
+            formatter = ctx.make_formatter()
+            with formatter.indentation():
+                indent = ' ' * formatter.current_indent
+                return f'{indent}{opts}', opt_help
 
 
 class OptionGroup:
     """Option group manages grouped (related) options
     """
 
-    def __init__(self, name: ty.Optional[str] = None, description: ty.Optional[str] = None, *,
-                 requied: bool = False) -> None:
+    def __init__(self, name: ty.Optional[str] = None, help: ty.Optional[str] = None, *,
+                 required: bool = False) -> None:
         self._name = name if name else ''
-        self._description = description if description else ''
-        self._requied = requied
+        self._help = help if help else ''
+        self._required = required
         self._options = {}
+        self._fake_helper_options = {}
 
     @property
     def name(self) -> str:
@@ -76,11 +79,11 @@ class OptionGroup:
 
     @property
     def description(self) -> str:
-        """Returns the group description or empty string if it was not set
+        """Returns the group help or empty string if it was not set
 
-        :return: group description
+        :return: group help
         """
-        return self._description
+        return self._help
 
     @property
     def required(self) -> bool:
@@ -90,7 +93,11 @@ class OptionGroup:
 
         :return: required flag
         """
-        return self._requied
+        return self._required
+
+    @property
+    def name_extra(self) -> ty.List[str]:
+        return ['required'] if self.required else []
 
     def get_default_name(self, ctx: click.Context) -> str:
         """Returns default name for the group
@@ -103,25 +110,30 @@ class OptionGroup:
 
         option_names = reversed(list(self.get_options(ctx)))
         option_names = '|'.join(option_names)
-        return f'({option_names})'
+        return f'{option_names}'
 
     def get_help_record(self, ctx: click.Context) -> ty.Tuple[str, str]:
         """Returns the help record for the group
 
         :param ctx: Click Context object
-        :return: the tuple of two fileds: (name, description)
+        :return: the tuple of two fileds: (name, help)
         """
+
         name = self.get_default_name(ctx)
         descr = self.description if self.description else ''
 
-        if self.required:
-            name += ' [required]'
+        extra = ', '.join(self.name_extra)
+        if extra:
+            extra = f'[{extra}]'
+
+        name = f'({name}) GROUP {extra}'
 
         return name, descr
 
     def option(self, *param_decls, **attrs):
         """Decorator attaches an grouped option to the command
         """
+
         def decorator(func):
             option_attrs = attrs.copy()
             option_attrs.setdefault('cls', GroupedOption)
@@ -132,9 +144,19 @@ class OptionGroup:
             self._check_decorated_order(func)
             func = click.option(*param_decls, group=self, **option_attrs)(func)
             self._option_memo(func)
+
+            # Add the fake invisible option to use for print nice help for grouped options
+            self._add_fake_helper_option(func)
+
             return func
 
         return decorator
+
+    def get_options(self, ctx: click.Context) -> dict:
+        return self._options.get(ctx.command.callback, {})
+
+    def get_fake_option(self, ctx: click.Context) -> ty.Optional[GroupedOption]:
+        return self._fake_helper_options.get(ctx.command.callback)
 
     def handle_parse_result(self, option: GroupedOption, ctx: click.Context, opts: dict) -> None:
         if not self.required:
@@ -152,20 +174,27 @@ class OptionGroup:
 
             raise click.UsageError(error_text, ctx=ctx)
 
-    def _check_decorated_order(self, func):
+    @staticmethod
+    def _get_callback_and_params(func):
         if isinstance(func, click.Command):
             params = func.params
             func = func.callback
         else:
             params = getattr(func, '__click_params__', [])
 
+        return func, params
+
+    def _check_decorated_order(self, func):
+        func, params = self._get_callback_and_params(func)
+
         if not params or func not in self._options:
             return
 
         last_param = params[-1]
+        fake_option = self._fake_helper_options[func]
         options = self._options[func]
 
-        if last_param.name not in options:
+        if last_param.name != fake_option.name and last_param.name not in options:
             hint_list = last_param.opts or [last_param.human_readable_name]
 
             raise ValueError((
@@ -174,34 +203,39 @@ class OptionGroup:
                 f"Check decorator position for {hint_list} option."
             ))
 
+    def _add_fake_helper_option(self, func):
+        callback, params = self._get_callback_and_params(func)
+
+        if callback not in self._fake_helper_options:
+            fake_opt_name = ''.join(random.choices(string.ascii_lowercase, k=_FAKE_OPT_NAME_LEN))
+            func = click.option(f'--{fake_opt_name}',
+                                group=self, cls=GroupedOption, expose_value=False)(func)
+
+            _, params = self._get_callback_and_params(func)
+            self._fake_helper_options[callback] = params[-1]
+
+        fake_option = self._fake_helper_options[callback]
+        last_option = params[-1]
+
+        if fake_option.name != last_option.name:
+            # Hold fake option on the top of the option group
+            fake_index = params.index(fake_option)
+            params[-1], params[fake_index] = params[fake_index], params[-1]
+
     def _option_memo(self, func):
-        if isinstance(func, click.Command):
-            params = func.params
-            func = func.callback
-        else:
-            params = func.__click_params__
+        func, params = self._get_callback_and_params(func)
 
         option: GroupedOption = params[-1]
         self._options.setdefault(func, {})[option.name] = option
-
-    def get_options(self, ctx: click.Context) -> dict:
-        return self._options.get(ctx.command.callback, {})
 
 
 class MutuallyExclusiveOptionGroup(OptionGroup):
     """Option group with mutually exclusive behavior for grouped options
     """
 
-    def get_help_record(self, ctx: click.Context) -> ty.Tuple[str, str]:
-        name = self.get_default_name(ctx)
-        descr = self.description if self.description else ''
-
-        if self.required:
-            name += ' [mutually_exclusive, required]'
-        else:
-            name += ' [mutually_exclusive]'
-
-        return name, descr
+    @property
+    def name_extra(self) -> ty.List[str]:
+        return super().name_extra + ['mutually_exclusive']
 
     def handle_parse_result(self, option: GroupedOption, ctx: click.Context, opts: dict) -> None:
         super().handle_parse_result(option, ctx, opts)
